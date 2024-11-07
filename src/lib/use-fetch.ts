0852f388a3f82a758a85f8fb390554f9ebe981f8
DellaBitta <drsanta@google.com>
2024-11-07 10:19:49 -0500

    log getAbout

--- src/lib/use-fetch.ts
+++ src/lib/use-fetch.ts
@@ -3,17 +3,21 @@ import { getFirebase } from "./use-firebase";
 
 
 export const getAbout = async () => {
-
+    console.log("in getAbout");
     if (isBrowser) {
 
+        console.log("isBrowser");
         const { auth } = await getFirebase();
+        console.log("Auth: ", auth);
 
         if (!auth?.currentUser) {
             throw 'Not Logged in!';
         }
 
         const token = await auth.currentUser.getIdToken();
+        console.log("idtoken: ", token);
 
+        console.log("calling fetch /about");
         const result = await fetch('/about/', {
             method: 'POST',
             body: '',
@@ -21,6 +25,7 @@ export const getAbout = async () => {
                 'Authorization': 'Bearer ' + token
             }
         });
+        console.log("fetch about result: ", result );
 
         if (!result.ok) {
             const e = await result.json();
@@ -29,7 +34,8 @@ export const getAbout = async () => {
         }
 
         const about = await result.json();
-
+        console.log("about from json; ", about);
+        
         return about.data as AboutDoc;
     }
 
