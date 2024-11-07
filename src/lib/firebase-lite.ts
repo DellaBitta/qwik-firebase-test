08a0b07dfd5f09dbb652fc1caaba283e0d8dbfeb
DellaBitta <drsanta@google.com>
2024-11-07 11:05:30 -0500

    remove dynamic imports

--- src/lib/firebase-lite.ts
+++ src/lib/firebase-lite.ts
@@ -5,9 +5,13 @@ const firebase_config = JSON.parse(
     import.meta.env.PUBLIC_FIREBASE_CONFIG
 );
 
-const importFirestoreLite = async () => await import('firebase/firestore/lite');
-const importFirebaseApp = async () => await import('firebase/app');
-const importFirebaseAuth = async () => await import('firebase/auth');
+//const importFirestoreLite = async () => await import('firebase/firestore/lite');
+//const importFirebaseApp = async () => await import('firebase/app');
+//const importFirebaseAuth = async () => await import('firebase/auth');
+import { initializeServerApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore/lite';
+
 
 
 export const firebaseServer = async (request: Request) => {
@@ -16,22 +20,25 @@ export const firebaseServer = async (request: Request) => {
 
     
     console.log("AuthIdToken in request ", authIdToken);
-    const firebaseApp = await importFirebaseApp();
-    const firebaseAuth = await importFirebaseAuth();
+    //const firebaseApp = await importFirebaseApp();
+    //const firebaseAuth = await importFirebaseAuth();
 
 
-    const serverApp = firebaseApp.initializeServerApp(firebase_config, {
+    //const serverApp = firebaseApp.initializeServerApp(firebase_config, {
+    const serverApp = initializeServerApp(firebase_config, {
         authIdToken
     });
     console.log("serverApp: ", serverApp);
 
 
-    const serverAuth = firebaseAuth.getAuth(serverApp);
+    //const serverAuth = firebaseAuth.getAuth(serverApp);
+    const serverAuth = getAuth(serverApp);
     console.log("serverAuth: ", serverAuth);
     await serverAuth.authStateReady();
 
-    const firestoreLite = await importFirestoreLite();
-    const serverDB = firestoreLite.getFirestore(serverApp);
+    //const firestoreLite = await importFirestoreLite();
+    //const serverDB = firestoreLite.getFirestore(serverApp);
+    const serverDB = getFirestore(serverApp);
 
     return {
         serverAuth,
